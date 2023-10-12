5b96a4e6dd76f9ab2ea1c06f5e09331d1a8db37c
John Pierre Mendoza <johnpierremendoza@gmail.com>
2023-10-12 16:52:24 -0400

    update config

--- lua/config/keyremap.lua
+++ lua/config/keyremap.lua
@@ -2,10 +2,10 @@
 vim.keymap.set('n', '<leader>e', require("oil").toggle_float);
 
 -- Telescope keybinds
-vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files)
-vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep)
-vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers)
-vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags)
+vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = "Find files" })
+vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = "Live grep" })
+vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers, { desc = "Search buffers" })
+vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = "Search help" })
 
 -- Buffer keybinds
 vim.keymap.set('n', ']b', '<cmd>bn<cr>')
