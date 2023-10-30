4b6edfacef141575281cec21e191e9e9e5d34781
John Pierre Mendoza <johnpierremendoza@gmail.com>
2023-10-30 13:26:56 -0400

    update config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -121,6 +121,11 @@ return {
 	},
 
 	{
+		'windwp/nvim-ts-autotag',
+		opts = {}, -- this is equalent to setup({}) function
+	},
+
+	{
 		"neovim/nvim-lspconfig",
 		event = {"BufReadPre", "BufNewFile"},
 		dependencies = {
@@ -222,6 +227,12 @@ return {
 			"rafamadriz/friendly-snippets",
 		},
 		config = function()
+			local has_words_before = function()
+				unpack = unpack or table.unpack
+				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+				return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+			end
+
 			local cmp = require("cmp")
 			local luasnip = require("luasnip")
 
@@ -244,6 +255,26 @@ return {
 					['<C-Space>'] = cmp.mapping.complete(),
 					['<C-e>'] = cmp.mapping.abort(),
 					['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						elseif has_words_before() then
+							cmp.complete()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 				}),
 				sources = cmp.config.sources({
 					{ name = 'nvim_lsp' },
