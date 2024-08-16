809df1a49205556ed5bd6ba0d59dc5b22ddbf02c
John Pierre Mendoza <johnpierremendoza@gmail.com>
2024-08-16 10:30:24 +0800

    update config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -80,10 +80,7 @@ return {
   {
     "max397574/better-escape.nvim",
     config = function()
-      require("better_escape").setup({
-        mapping = {"jk", "jj", "kk", "kj"},
-        timeout = 250,
-      })
+      require("better_escape").setup()
     end,
   },
 
