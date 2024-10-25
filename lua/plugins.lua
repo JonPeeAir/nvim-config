2e8431dee8d6fb3ded327dd52b016040453b9a4a
John Pierre Mendoza <johnpierremendoza@gmail.com>
2024-10-25 12:52:44 +0800

    Update config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -109,7 +109,46 @@ return {
   {
     "max397574/better-escape.nvim",
     config = function()
-      require("better_escape").setup()
+      require("better_escape").setup({
+        timeout = 115,
+        default_mappings = false,
+        mappings = {
+            i = {
+                j = {
+                    -- These can all also be functions
+                    k = "<Esc>",
+                    -- j = "<Esc>",
+                },
+                k = {
+                    -- These can all also be functions
+                    -- k = "<Esc>",
+                    j = "<Esc>",
+                },
+            },
+            c = {
+                j = {
+                    k = "<Esc>",
+                    j = "<Esc>",
+                },
+            },
+            t = {
+                j = {
+                    k = "<C-\\><C-n>",
+                },
+            },
+            v = {
+                -- j = {
+                --     k = "<Esc>",
+                -- },
+            },
+            s = {
+                j = {
+                    k = "<Esc>",
+                },
+            },
+        },
+
+      })
     end,
   },
 
